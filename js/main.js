22823c7602038ac5135cb468f38fef095c1392a0
Sakshi Bisht <sakshibishitbb_cse17@its.edu.in>
2021-02-05 19:27:34 +0530

    Day 1

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function() {
+        $nav.toggleClass('collapse');
+    })
+
+    // owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 1000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+
+    })
+});
