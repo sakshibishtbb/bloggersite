df489f8c7c23af44ef8ed433e90d53437bb747d6
Sakshi Bisht <sakshibishitbb_cse17@its.edu.in>
2021-02-09 18:59:55 +0530

    third commit

--- js/main.js
+++ js/main.js
@@ -17,5 +17,11 @@ $(document).ready(function() {
         nav: true,
         navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
 
+    });
+
+    $('.move-up span').click(function() {
+        $('html ,body').animate({
+            scrollTop: 0
+        }, 1000);
     })
 });
